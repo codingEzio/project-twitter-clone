f591f31cde1d6b42a53813b801fdd4a5f5c2b3fe
!Mensch <assassinste@gmail.com>
2022-09-25 21:53:47 +0800

    dev: Implementing responsive layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ import SidebarRow from './SidebarRow'
 
 export const Sidebar = () => {
   return (
-    <div className='flex flex-col col-span-2'>
+    <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
         <img className='h-10 w-10' src="https://via.placeholder.com/350x350?text=Sidebar" alt="Placeholder image which its size is 150px" />
 
         <SidebarRow Icon={HomeIcon} title="Home" />
