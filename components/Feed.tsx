c1acc0b52737801493e510be4d2dc3206753babe
!Mensch <assassinste@gmail.com>
2022-09-25 11:43:17 +0800

    feat: Feed foundation (only an explicit refresh button as of now)

---
+++ components/Feed.tsx
@@ -0,0 +1,11 @@
+import { RefreshIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+export const Feed = () => {
+  return (
+    <div className='flex items-center justify-between'>
+        <h1 className='p-5 pb-0 text-xl font-bold'>Refresh</h1>
+        <RefreshIcon className='mr-5 mt-5 h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125' />
+    </div>
+  )
+}
