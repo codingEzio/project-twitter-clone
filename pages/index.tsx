400f64e186d5bb5172783e3a31060e9974facf28
!Mensch <assassinste@gmail.com>
2022-09-25 22:08:04 +0800

    dev: Responsive scrolling for twitter timeline content

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
         <title>Twitter Clone</title>
         <link rel="icon" href="/favicon.ico" />
