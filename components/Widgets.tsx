90746bdbea6e4bc73585d2c3c3e1582b18b0c7e4
!Mensch <assassinste@gmail.com>
2022-09-25 21:46:26 +0800

    dev: Dividing the layout into nine parts

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -4,7 +4,7 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 function Widgets() {
   return (
-    <div>
+    <div className='col-span-2 mt-2 px-2'>
       <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full">
         <SearchIcon className="h-5 w-5 text-gray-400" />
         <input
