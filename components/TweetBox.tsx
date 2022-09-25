2079bd3f89c4389846c3fdf2cf10a51e312731e4
!Mensch <assassinste@gmail.com>
2022-09-25 23:04:59 +0800

    feat: Tweetbox foundation (avatar, input and buttons)

---
+++ components/TweetBox.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  PhotographIcon,
+  SearchCircleIcon,
+  EmojiHappyIcon,
+  CalendarIcon,
+  LocationMarkerIcon,
+} from '@heroicons/react/outline';
+
+export const TweetBox = () => {
+  return (
+    <div className="flex space-x-2 p-5">
+      <img
+        src="https://via.placeholder.com/350x350?text=Avatar"
+        alt="Placeholder user avatar"
+        className="h-14 w-14 object-cover rounded-full mt-4"
+      />
+
+      <div className="flex flex-1 items-center pl-2">
+        <form className="flex flex-1 flex-col">
+          <input
+            type="text"
+            placeholder="What's Happening?"
+            className="h-24 w-full text-xl outline-none placeholder:text-xl"
+          />
+          <div className="flex items-center">
+            <div className="flex flex-1 space-x-2 text-twitter">
+              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
+              <SearchCircleIcon className="h-5 w-5" />
+              <EmojiHappyIcon className="h-5 w-5" />
+              <CalendarIcon className="h-5 w-5" />
+              <LocationMarkerIcon className="h-5 w-5" />
+            </div>
+
+            <button className="bg-twitter px-5 font-bold text-white rounded-full">
+              Tweet
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
