41233d8ab06068f59d8b593c4e701b1bd0952071
!Mensch <assassinste@gmail.com>
2022-09-25 13:02:28 +0800

    feat: Integrating twitter timeline into project

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,13 +1,26 @@
-import { SearchIcon } from '@heroicons/react/outline'
-import React from 'react'
+import { SearchIcon } from '@heroicons/react/outline';
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 function Widgets() {
   return (
-    <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full'>
-        <SearchIcon className='h-5 w-5 text-gray-400' />
-        <input type='text' placeholder='Search Twitter' className='flex-1 bg-transparent outline-none' />
+    <div>
+      <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="flex-1 bg-transparent outline-none"
+        />
+      </div>
+
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="elonmusk"
+        options={{ height: 500 }}
+      />
     </div>
-  )
+  );
 }
 
-export default Widgets
+export default Widgets;
