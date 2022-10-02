f47caa2f063733ec5526e10d6f7dc1bc1541e16c
!Mensch <assassinste@gmail.com>
2022-10-02 08:04:25 +0800

    feat: OAuth authentication for our app (sometime it works)

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,21 +10,32 @@ import {
     HomeIcon
 } from '@heroicons/react/outline'
 import SidebarRow from './SidebarRow'
+import { useSession, signOut, signIn } from 'next-auth/react';
 
 export const Sidebar = () => {
-  return (
-    <div className='flex flex-col col-span-2 items-center px-4 md:items-start'>
-        <img className='m-3 h-10 w-10' src="https://via.placeholder.com/350x350?text=Sidebar" alt="Placeholder image which its size is 150px" />
+  const { data: session } = useSession();
 
-        <SidebarRow Icon={HomeIcon} title="Home" />
-        <SidebarRow Icon={HashtagIcon} title="Explore" />
-        <SidebarRow Icon={BellIcon} title="Notifications" />
-        <SidebarRow Icon={MailIcon} title="Messages" />
-        <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
-        <SidebarRow Icon={CollectionIcon} title="Lists" />
-        <SidebarRow Icon={UserIcon} title="Sign In" />
-        <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+  return (
+    <div className="flex flex-col col-span-2 items-center px-4 md:items-start">
+      <img
+        className="m-3 h-10 w-10"
+        src="https://via.placeholder.com/350x350?text=Sidebar"
+        alt="Placeholder image which its size is 150px"
+      />
+
+      <SidebarRow Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={HashtagIcon} title="Explore" />
+      <SidebarRow Icon={BellIcon} title="Notifications" />
+      <SidebarRow Icon={MailIcon} title="Messages" />
+      <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SidebarRow Icon={CollectionIcon} title="Lists" />
+      <SidebarRow
+        Icon={UserIcon}
+        title={session ? 'Sign Out' : 'Sign In'}
+        onClick={session ? signOut : signIn}
+      />
+      <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
     </div>
-  )
-}
+  );
+};
 
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,17 +1,23 @@
 import React, { SVGProps } from 'react'
 
 interface Props {
-    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
-    title: String
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  title: String;
+  onClick?: () => {};
 }
 
-function SidebarRow({Icon, title}: Props) {
+function SidebarRow({ Icon, title, onClick }: Props) {
   return (
-    <div className='group flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200'>
-        <Icon className="h-6 w-6" />
-        <p className='group-hover:text-twitter hidden md:inline-flex text-base font-light lg:text-xl'>{title}</p>
+    <div
+      onClick={() => onClick?.()}
+      className="group flex max-w-fit items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 transition-all duration-200"
+    >
+      <Icon className="h-6 w-6" />
+      <p className="group-hover:text-twitter hidden md:inline-flex text-base font-light lg:text-xl">
+        {title}
+      </p>
     </div>
-  )
+  );
 }
 
 export default SidebarRow
