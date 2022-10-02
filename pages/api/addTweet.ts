40bbc0a8980c250c063f5f4056688c232b90704c
!Mensch <assassinste@gmail.com>
2022-10-02 10:58:07 +0800

    feat: Constructing tweet body and the fetching URL

---
+++ pages/api/addTweet.ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { TweetBody } from '../../typing';
+
+type Data = {
+  message: string;
+};
+
+// Accessible from http://localhost:3000/api/hello
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const data: TweetBody = JSON.parse(req.body);
+  const mutations = {
+    mutations: [
+      {
+        create: {
+          _type: 'tweet',
+          text: data.text,
+          username: data.username,
+          blockTweet: false,
+          profileImg: data.profileImg,
+          image: data.image,
+        },
+      },
+    ],
+  };
+
+  const apiEndPoint = 'https://'.concat(
+    `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
+    '.api.sanity.io',
+    '/v2021-10-21/data',
+    '/mutate',
+    `/${process.env.NEXT_PUBLIC_SANITY_DATESET}`,
+  );
+
+  const result = await fetch(apiEndPoint, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${process.env.SANITY_API_TOKEN}`,
+    },
+    body: JSON.stringify(mutations),
+    method: 'POST',
+  });
+
+  const json = await result.json();
+
+  res.status(200).json({ message: 'Added' });
+}
